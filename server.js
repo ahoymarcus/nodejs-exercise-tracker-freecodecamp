0e8f5fdbd9c0fdb7c244cd1327f806f0d60b3eb1
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-10-08 21:06:17 -0300

    Melhorar estilo e estrutura da home page

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ app.use(cors())
 app.use(express.static('public'))
 
 
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+
+app.get('/api/users', function(req, res) {
+	
+});
+
+
 
 
 
@@ -24,6 +30,12 @@ app.get('/', (req, res) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+
+
+
+
+
 
 
 
